1044cc72382bbf3d5b58e44e1f1fe7d7ba1c0c8e
Umar <Umarmail.ru>
2022-01-29 15:57:59 +0500

    3rd commit by Umar

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@
 // })
 
 
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.querySelector('.fixed-navbar').classList.add('actived')
+  } else {
+    document.querySelector('.fixed-navbar').classList.remove('actived')
+  }
+}
+
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.fixed-nav-list ul')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('actived')
+  nav.classList.toggle('actives')
+})
+// const burgerClick = () => {
+// }
